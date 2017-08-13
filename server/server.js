ac0503309201959e11906b7d3cf5b6dfb2265fc8
Vishal Desai <Vishal Desai>
2017-08-13 23:12:28 +0530

    Added Get Routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 
-
 const express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 app.use(bodyParser.json());
@@ -27,4 +27,49 @@ app.post('/todos', (req,res) => {
       errorMessage: err
     })
   })
-})
+});
+
+//GET : /todos - Get All Todo
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.status(200).send({
+      todos
+    })
+  }, (err) => {
+    res.status(400).send({
+      errorCode: 400,
+      errorMessage: err
+    });
+  });
+});
+
+//GET : /todos/:id - Get Specific Todo
+
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(400).send({
+      errorCode: 400,
+      errorMessage: 'Invalid Object ID'
+    });
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send({
+        errorCode: 404,
+        errorMessage: 'Data not found'
+      });
+    }
+
+    res.status(200).send(JSON.stringify(todo,undefined, 2));
+  }).catch((err) => {
+    res.status(400).send({
+      errorCode: 400,
+      errorMessage: {err}
+    });
+  });
+
+});
